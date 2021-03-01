8af57a24a4f43b8463cc7ba26aa0004970f5501d
lcastrovarela <lcastrovarela@gmail.com>
2021-03-01 04:38:49 +0000

    finished random playing card

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//write your code here
+let numeros = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+let palos = ["\u2660", "\u2663", "\u2665", "\u2666"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let random = Math.floor(Math.random() * 4);
+  let random2 = Math.floor(Math.random() * 14);
+
+  let palo = palos[random];
+  let numero = numeros[random2];
+
+  let elemento1 = document.getElementById("top-suit");
+  let elemento2 = document.getElementById("number");
+  let elemento3 = document.getElementById("bottom-suit");
+
+  if (palo === "\u2665") {
+    elemento1.style.color = "red";
+    elemento3.style.color = "red";
+  } else if (palo === "\u2666") {
+    elemento1.style.color = "red";
+    elemento3.style.color = "red";
+  }
+
+  elemento1.innerHTML = palo;
+  elemento2.innerHTML = numero;
+  elemento3.innerHTML = palo;
 };
